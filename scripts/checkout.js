be87b0a2b187eae374a86531b9bdb566f74e8ab7
johnson2004412 <runjiazhang.nz@gmail.com>
2024-12-10 12:09:12 +0800

    Implement removeFromCart function and update checkout.js to handle item deletion

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 
 let cartSummaryHTML = "";
 
@@ -14,7 +15,6 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  console.log(matchingProduct);
   cartSummaryHTML += `
     <div class="cart-item-container">
           <div class="delivery-date">
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                   Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                   Delete
                 </span>
               </div>
@@ -89,3 +89,12 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".order-summary").innerHTML = cartSummaryHTML;
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log("cart", cart);
+  });
+});
+
+
