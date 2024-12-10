e8cffc7fa544e2da1765cf819573294cb201b53d
johnson2004412 <runjiazhang.nz@gmail.com>
2024-12-10 15:56:04 +0800

    Implement payment summary rendering and refactor order summary; enhance delivery option retrieval and currency formatting

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,9 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { getProduct, products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -10,23 +11,12 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if (productId === product.id) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,5 @@
 export function formatCurrency(priceCents) {
-  priceCents = (priceCents / 100).toFixed(2);
+  priceCents = (Math.round(priceCents) / 100).toFixed(2);
   return priceCents
 }
 
