f791ba215d78329cbc9a57fe5c0ff28a6f47ac09
johnson2004412 <runjiazhang.nz@gmail.com>
2024-12-11 10:28:05 +0800

    Remove Jasmine test files and implement new currency formatting tests

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("Normal functionality testing", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  }),
+    it("Boundary value testing", () => {
+      expect(formatCurrency(0)).toEqual("0.00");
+    }),
+    it("Extreme value testing", () => {
+      expect(formatCurrency(1000000)).toEqual("10000.00");
+    }),
+    it("Decimal rounding testing", () => {
+      expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+});
