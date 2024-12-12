9a783029cd0dd6ad370f554f7e81af8f30406213
johnson2004412 <runjiazhang.nz@gmail.com>
2024-12-12 09:56:13 +0800

    Add loadCart function to fetch cart data and integrate with checkout process

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,22 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
 
-loadProducts(() => {
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
