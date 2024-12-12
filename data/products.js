519e98312862594a9b78d9df759dde5e95d47ada
johnson2004412 <runjiazhang.nz@gmail.com>
2024-12-12 10:03:21 +0800

    Implement loadProductsFetch function to retrieve product data and update checkout process

--- data/products.js
+++ data/products.js
@@ -60,7 +60,26 @@ class Clothing extends Product {
 
 
 export let products = [];
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products')
+    .then(response => response.json())
+    .then(data => {
+      products = data.map((productDetails) => {
+        if (productDetails.type === 'clothing') {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+      console.log('load products');
+    });
+  return promise;
+}
 
+loadProductsFetch().then(() => {
+  console.log('next Step');
+});
+
+/*
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', () => {
@@ -76,7 +95,7 @@ export function loadProducts(fun) {
   xhr.open('GET', 'https://supersimplebackend.dev/products', true);
   xhr.send();
 }
-
+*/
 
 
 /*
